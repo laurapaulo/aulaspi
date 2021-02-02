38a5dcf8733b5b3c747e4c8825ba6f3d98a29810
Laura Oliveira <laurapaulo2017@gmail.com>
2021-02-02 09:57:29 -0300

    controller de eventos

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -1,5 +1,24 @@
 package ifrn.pi.eventos.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class EventosController {
+	
+	@RequestMapping ("/eventos/form")
+	public String form() {
+		return "formEvento";	
+	}
+	
+	@RequestMapping(value= "/eventos/form",  method = RequestMethod.POST)
+	public String envioForm(String nome, String local, String data, String horario) {
+		System.out.println(nome + local + data + horario);
+		return "formularioEnviado";
+		
+	}
+	
+	
 
 }
